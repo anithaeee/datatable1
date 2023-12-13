243ee8cff6ffe9f83d6227d1c6a4ccc4ed82d954
anithaeee <anithaeee224@gmail.com>
2023-12-13 14:13:17 +0800

    13th dec

---
+++ src/Child.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react';
+import {DataTable} from 'primereact/datatable'
+const Child = (props) => {
+  return (
+    <div>Child</div>
+  )
+}
+
+export default Child;
