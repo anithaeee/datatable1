6dcf72da7b4ee9549e0c656def62ee3ebe65ba3e
anithaeee <anithaeee224@gmail.com>
2023-12-14 23:52:14 +0800

    dec14th

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { useState } from 'react';
 import Child from './Child';
 function App(props) {
   const[name,setname]= useState("");
-   const data =[{name:'Anitha', Age:29,Place:"Hyderabad"},
-                {name:'Ashiwini', Age:24,Place:"Hyderabad"},
-                 {name:'Ausha', Age:24,Place:"Hyderabad"}]
+   const[data,setdata]= useState([]);
   return (
    <>
      <Child griddata={data}/>
--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import {DataTable} from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { ProductService } from './ProductService';
 const Child = (props) => {
     const[name,setname] = useState("");
+    const[data, setdata] = useState([]);
+    useEffect(()=>{
+        ProductService.getProducts().then(data=>setdata(data));
+    });
   return (
     <div>
-       <DataTable value={props.griddata}>
-        <Column field='name' header='Name'></Column>
-        <Column field='Age' header='Age'></Column>
-        <Column field='Place' header='Place'></Column>
+       <DataTable value={data}>
+        <Column field='id' header='id'></Column>
+        <Column field='code' header='code'></Column>
+        <Column field='name' header='name'></Column>
+        <Column field='description' header='description'></Column>
+        <Column field='image' header='image'></Column>
+        <Column field='category' header='category'></Column>
+        <Column field='quantity' header='quantity'></Column>
+        <Column field='inventoryStatus' header='inventoryStatus'></Column>
+        <Column field='rating' header='rating'></Column>
        </DataTable>
     </div>
   )
